2b9afb38b09aff5f00bf867d668760e3b39297c9
westwe6 <westwe6@users.noreply.github.com>
2016-01-18 15:54:48 -0500

    Update SampleList_Generator_QIIME.R

--- SampleList_Generator_QIIME.R
+++ SampleList_Generator_QIIME.R
@@ -5,11 +5,11 @@
 
 setwd("~/Desktop/20150803_16S-18S_PE/16S")
 files16S=list.files(getwd())
-files16S=files16S[-c(1,242)]
+#files16S=files16S[-c(1,242)]
 write.table(files16S,"16S_SampleList_fastq.txt",sep="\t",row.names=FALSE)
 
 
 setwd("~/Desktop/20150803_16S-18S_PE/18S")
 files18S=list.files(getwd())
-files18S=files18S[-c(1,242)]
+#files18S=files18S[-c(1,242)]
 write.table(files18S,"18S_SampleList_fastq.txt",sep="\t",row.names=FALSE)
